c1c8435fd850a752c889d030c8074deadc34c595
Jaesung Lee <jason0853@gmail.com>
2020-06-07 14:49:06 +0900

    build

---
+++ build/precache-manifest.b22c3d1824b228973a8f7bd8de68b367.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3254aaf29422828846096afb92b06f9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2ab851fda3c5db943e6",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0cd1a27a0462168175ce",
+    "url": "/static/js/2.4abb44e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4abb44e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2ab851fda3c5db943e6",
+    "url": "/static/js/main.0a61bb07.chunk.js"
+  },
+  {
+    "revision": "03400c5030460c9402dc",
+    "url": "/static/js/runtime-main.d6441d4b.js"
+  }
+]);
