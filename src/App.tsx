3d31bbd3601a0f653bd417083597fc7f3ed9dae2
Jaesung Lee <jason0853@gmail.com>
2020-06-16 00:58:49 +0900

    13

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      EC2 Instance and NGINX and Jenkins 12
+      EC2 Instance and NGINX and Jenkins 13
     </div>
   );
 }
