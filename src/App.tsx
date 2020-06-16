aa9b0e817374e0fdc7c821979c8bb30483f23e9b
LJ <lj@happytalk.io>
2020-06-16 13:17:55 +0900

    test5

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      EC2 Instance and NGINX and Jenkins 16
+      EC2 Instance and NGINX and Jenkins 17
     </div>
   );
 }
