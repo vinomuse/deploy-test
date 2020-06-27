0c4547dbdf75aa00fc308404be8eda6b91f6e916
Jaesung Lee <jason0853@gmail.com>
2020-06-28 02:08:39 +0900

    archive 12

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -4,8 +4,9 @@ echo 'Start deploying'
 
 set -x
 cd /home
-ssh -i "my_aws_key.pem" ubuntu@ec2-15-165-161-155.ap-northeast-2.compute.amazonaws.com
 ls -al
+# ssh -i "my_aws_key.pem" ubuntu@ec2-15-165-161-155.ap-northeast-2.compute.amazonaws.com
+# ls -al
 set +x
 
 echo 'Deployed!'
