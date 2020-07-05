190bf6aae65eb36c5ae1cc50d72b65e620561bd6
Jaesung Lee <jason0853@gmail.com>
2020-07-05 15:58:56 +0900

    real test 6

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      EC2 Instance and NGINX and Jenkins 5
+      EC2 Instance and NGINX and Jenkins 6
     </div>
   );
 }
